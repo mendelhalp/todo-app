703ae5fce3fec7354597c037f6efa799b2325f09
Menachem Halperin <mendelhalp@gmail.com>
2021-01-05 00:13:08 +0200

    filter the shown list by filterType state

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -45,17 +45,7 @@ const ToDoPage = () => {
         localStorage.tasks = JSON.stringify(newTasks);
         setTasks(newTasks);
     }
-
-    //convert tasks from plain objects to TaskModel object
-    //and render TaskItem for each
-    let taskList;
-    if (tasks) {
-        taskList = tasks.map((task, index) =>
-            <TaskItem key={index} index={index} text={task.text} isChecked={task.isChecked}
-                onSelect={() => setChecked(index)} handleDeleteItem={(index) => deleteItem(index)} />
-        )
-    }
-
+    
     // calculate the amount of unchecked items left 
     const itemsLeft = () => {
         let left = 0;
@@ -66,6 +56,23 @@ const ToDoPage = () => {
         }
         return left;
     }
+
+    //filter the list by filterType
+    let tasksList = [...tasks];
+    if (tasks) {
+        if (filterType === 'Active') {
+            tasksList = tasks.filter(task => !task.isChecked)
+        } else if (filterType === 'Complited'){
+            tasksList = tasks.filter(task => task.isChecked)
+        }
+        
+        //convert tasks from plain objects to TaskModel object
+        //and render TaskItem for each
+        tasksList = tasksList.map((task, index) =>
+            <TaskItem key={index} index={index} text={task.text} isChecked={task.isChecked}
+                onSelect={() => setChecked(index)} handleDeleteItem={(index) => deleteItem(index)} />
+        )        
+    }    
 
     return (
         <div className="p-todo">
@@ -80,7 +87,7 @@ const ToDoPage = () => {
                     </Col>
                     <Col className="center">
                         <div className="task-list mb-4">
-                            {taskList}
+                            {tasksList}
                         </div>
                     </Col>
                     <Col className="center">
@@ -88,9 +95,9 @@ const ToDoPage = () => {
                             <div className="between">
                                 <span>{itemsLeft()} items left</span>
                                 <ToggleButtonGroup name="select-filter">
-                                    <Button className="m-1" variant="light" active={(filterType === 'All') ? true : false}>All</Button>
-                                    <Button className="m-1" variant="light" active={(filterType === 'Active') ? true : false}>Active</Button>
-                                    <Button className="m-1" variant="light" active={(filterType === 'Complited') ? true : false}>Complited</Button>
+                                    <Button className="m-1" variant="light" onClick={() => {setFilterType('All')}} active={(filterType === 'All') ? true : false}>All</Button>
+                                    <Button className="m-1" variant="light" onClick={() => {setFilterType('Active')}} active={(filterType === 'Active') ? true : false}>Active</Button>
+                                    <Button className="m-1" variant="light" onClick={() => {setFilterType('Complited')}} active={(filterType === 'Complited') ? true : false}>Complited</Button>
                                 </ToggleButtonGroup>
                             </div>
                         }
