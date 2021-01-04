b2377546672a3d5117a0f2e0c1fcfc8f6bea50ae
Menachem Halperin <mendelhalp@gmail.com>
2021-01-04 11:33:02 +0200

    set the layout

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -1,22 +1,36 @@
-import './ToDoPage.scss';
+import { Button, ButtonGroup, Col, Container, Row, ToggleButtonGroup } from 'react-bootstrap';
+import './ToDoPage.css';
 
 const ToDoPage = () => {
 
 
-
-    return(
+    return (
         <div className="p-todo">
-            <h1>Todos</h1>
-            <input type="text" placeholder="What's next?"/>
-            <div className="list">
-
-            </div>
-            <span>x items left</span>
-            <ul>
-                <li>All</li>
-                <li>Active</li>
-                <li>Complited</li>
-            </ul>
+            <Container>
+                <Row xs={1} className="justify-center">
+                    <Col className="center mt-5 mb-3">
+                        <h1>Todos</h1>
+                    </Col>
+                    <Col className="center mb-4">
+                        <input className="text-center" type="text" placeholder="What's next?" />
+                    </Col>
+                    <Col className="center">
+                        <div className="list">
+                            {/* {list} */}
+                        </div>
+                    </Col>
+                    <Col className="center">
+                        <div className="between">
+                            <span>x items left</span>
+                            <ToggleButtonGroup name="select-filter">
+                                <Button className="m-1" variant="light">All</Button>
+                                <Button className="m-1" variant="light">Active</Button>
+                                <Button className="m-1" variant="light">Complited</Button>
+                            </ToggleButtonGroup>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
